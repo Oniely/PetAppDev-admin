422c801528e503fb4324de69c18710e077a67282
Oniely <jabagatniel09@gmail.com>
2024-05-12 23:59:14 +0800

    dashboard

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -20,17 +20,40 @@ const Home = async () => {
 					{userData.companyName}
 					<span className="font-light">'s Dashboard</span>
 					<br />
-
 				</h1>
 				<BreadCrumbs />
 			</header>
 			<hr className="-mt-8 mb-6" />
 			<section className="flex flex-col gap-8">
 				<div className="grid grid-cols-4 max-lg:grid-cols-2 gap-2">
-					<DashboardCard className="bg-[#D6EDFF]" title="Appointments" image_url="/images/calendar.svg" data="10" href="/services" />
-					<DashboardCard className="bg-[#ACD7EC]" title="Active Services" image_url="/images/services.svg" data="10" href="/services" />
-					<DashboardCard className="bg-[#79B4A9]" title="Inbox" image_url="/images/calendar.svg" data="10" href="/services" />
-					<DashboardCard className="bg-[#84DCC6]" title="Earnings" image_url="/images/calendar.svg" data="$100" href="/services" />
+					<DashboardCard
+						className="bg-[#D6EDFF]"
+						title="Appointments"
+						image_url="/images/calendar.svg"
+						data="10"
+						href="/services"
+					/>
+					<DashboardCard
+						className="bg-[#ACD7EC]"
+						title="Active Services"
+						image_url="/images/services.svg"
+						data="10"
+						href="/services"
+					/>
+					<DashboardCard
+						className="bg-[#79B4A9]"
+						title="Inbox"
+						image_url="/images/calendar.svg"
+						data="10"
+						href="/services"
+					/>
+					<DashboardCard
+						className="bg-[#84DCC6]"
+						title="Earnings"
+						image_url="/images/calendar.svg"
+						data="$100"
+						href="/services"
+					/>
 				</div>
 				<div></div>
 			</section>
