ea81b2b6e18e2de683a0a73842156f895200ba74
Oniely <jabagatniel09@gmail.com>
2024-05-21 17:04:39 +0800

    loading lazy load with image

--- src/components/shared/Loading.tsx
+++ src/components/shared/Loading.tsx
@@ -8,8 +8,7 @@ const Loading = ({ loading }: Props) => {
     return (
         <section className={`fixed inset-0 h-screen bg-white/50 z-[1000] ${loading ? "block" : "hidden"}`}>
             <div className="w-full h-full flex-1 flex items-center justify-center">
-                <Image src='/images/dogo-loading.gif' alt="dogo" width={300} height={300}/>
-                {/* <div className="animate-bounce">LOADING...</div> */}
+                <Image src='/images/dogo-loading.gif' alt="dogo" width={300} height={300} unoptimized/>
             </div>
         </section>
     )
