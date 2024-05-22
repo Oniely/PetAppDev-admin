0960ccc3d516f446f5798d84ae50c5ce2a96cf1c
Heart <heartryan.eledia.s@southlandcollege.edu.ph>
2024-05-22 12:31:38 +0800

    pet loading

--- src/components/shared/Loading.tsx
+++ src/components/shared/Loading.tsx
@@ -8,7 +8,7 @@ const Loading = ({ loading }: Props) => {
     return (
         <section className={`fixed inset-0 h-screen bg-white/50 z-[1000] ${loading ? "block" : "hidden"}`}>
             <div className="w-full h-full flex-1 flex items-center justify-center">
-                <Image src='/images/dogo-loading.gif' alt="dogo" width={300} height={300} unoptimized/>
+                <Image src='/images/pet-loading.gif' alt="dogo" width={400} height={400} unoptimized />
             </div>
         </section>
     )
